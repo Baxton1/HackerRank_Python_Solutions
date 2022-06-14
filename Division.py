cab58ba6dbf6996ec7657b9148d6ee9a41416623
Baxton <75202813+baxton1@users.noreply.github.com>
2022-06-14 16:19:19 +0200

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # a//b it's floor it gives us the lowest value.
+    
+    floor = a//b
+    division = a/b
+    
+    print(floor)
+    print(division)
