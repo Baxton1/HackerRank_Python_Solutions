e89b56a0e8696ca9defc178d344d2d82be32cf4c
Baxton <75202813+baxton1@users.noreply.github.com>
2022-06-14 20:46:08 +0200

    Update Write a function.py

--- Write a function.py
+++ Write a function.py
@@ -5,9 +5,27 @@ def is_leap(year):
     if year % 4 == 0 and year % 400 == 0:
         return True
     elif year % 100 == 0:
-        return leap
+        return leap                 #this first code gives a wrong answer for the year (1992)
     else:
         return leap #(False)
 
 year = int(input('Enter Year: '))
+print(is_leap(year))
+
+#####################################################################
+
+def is_leap(year):
+    leap = False
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                return True
+            else:
+                return leap
+        else:
+            return True
+    else:
+        return leap
+
+year = int(input())
 print(is_leap(year))
